21e5e1706db1be0d4ebf6c7d2f3f9816acfbd4d7
ayoitssmit <137383577+ayoitssmit@users.noreply.github.com>
2025-01-04 15:03:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ from flask import Flask, render_template, request, redirect, url_for, send_from_
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
 
 online_users = {}
 
